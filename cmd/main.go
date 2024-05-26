3a5d85938aa1ae48aefb741bbee344d2ba3cd825
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-05-26 22:53:49 +0100

    setting up

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	//"simple-colly-example/crawler"
+
+	"github.com/LusBlack/Aragog/crawler"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/{vin}", crawler.CrawlerForEngine)
+
+	fmt.Println("We're Oscar Mike. localhost:8000/{vin}")
+	http.ListenAndServe(":8000", router)
+}
---
+++ crawler/crawlerForEngine.go
@@ -0,0 +1,22 @@
+package crawler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func CrawlerForEngine(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	vin := vars["vin"]
+
+	//create new colly collector
+	c := colly.NewCollector(
+		colly.AllowURLRevisit(),
+		colly.MaxDepth(100),
+	)
+
+	fmt.Fprintf(w, "You used this vin: %s", vin)
+
+}
